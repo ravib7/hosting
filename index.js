700cba8a3cd0fb614a633040261a7eb0f0f39112
ravib7 <ravibchaudhari7@gmail.com>
2025-02-21 15:02:58 +0530

    added frontend

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 const cookieParser = require("cookie-parser")
 const { log } = require("./middlewares/logger")
+const path = require("path")
 require("dotenv").config()
 
 const app = express()
@@ -13,12 +14,15 @@ app.use(cookieParser())
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }))
 
+app.use(express.static("dist"))
+
 app.use(log)
 
 app.use("/api/auth", require("./routes/auth.routes"))
 
 app.use("*", (req, res) => {
-    res.status(404).json({ message: "resource not found" })
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
+    // res.status(404).json({ message: "resource not found" })
 })
 
 mongoose.connect(process.env.MONGO_URL)
